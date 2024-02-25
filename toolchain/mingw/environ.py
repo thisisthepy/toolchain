1b61799b24b795315e96de3b705dc6f9ce07426d
IRACK (CHAEUN) <IRACK000@gmail.com>
2024-02-25 16:28:35 +0900

    Feat: Mingw Tar Cache

--- toolchain/mingw/environ.py
+++ toolchain/mingw/environ.py
@@ -3,9 +3,9 @@ from toolchain.mingw.extract_zst import extract_zst
 from toolchain.settings import Settings
 from toolchain.logger import info
 
-from os import remove, makedirs
+from os import makedirs, replace
 from os.path import exists
-from shutil import replace
+from glob import glob
 import requests
 
 
@@ -25,9 +25,17 @@ class MinGW(Environment):
         def task(*_, **__):
             result = None
             name = ""
+            rev_v = 1
+            
+            url, filename, ext = Settings.binary_url.mingw
+            makedirs(Settings.cache_dir, exist_ok=True)
+            listings = glob(f"{filename}*{ext}", root_dir=Settings.cache_dir)
+            rev_list = [int(li.replace(filename+"-", "").replace(ext, "")) for li in listings]
+            rev_list.sort()
+            if rev_list:
+                rev_v = rev_list[-1]
 
-            for rev in range(1, 10):
-                url, filename, ext = Settings.binary_url.mingw
+            for rev in range(rev_v, 10):
                 respond = requests.get(url + filename + f"-{rev}" + ext)
                 if respond.status_code == 200:
                     result = respond
@@ -42,14 +50,13 @@ class MinGW(Environment):
                                         + " Please check the url information.")
 
             file_path = cls.dist_dir + "/" + name
-            makedirs(cls.dist_dir)
+            makedirs(cls.dist_dir, exist_ok=True)
             info("Downloading minGW binary from MSYS2 repository...")
             with open(file_path, "wb+") as file:
                 file.write(result.content)
             info(f"Download complete: {file_path}")
 
             extract_zst(file_path, cls.dist_dir)
-            remove(file_path)
             replace(cls.dist_dir+"/mingw64", cls.dist_dir+"/python3")
 
         super().init(task)
